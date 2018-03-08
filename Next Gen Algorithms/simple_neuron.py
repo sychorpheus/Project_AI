50cd1923e8ac6a8e6e2c8797929b90db3b66e770
Neo <neo@localhost.localdomain>
2018-03-08 21:37:51 +0300

    new algos

---
+++ Next Gen Algorithms/simple_neuron.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Neuron:
+    def __init__(self, weights, bias, function):
+        self.weights = weights
+        self.bias = bias
+        self.function = function
+
+    def forward(self, inputs):
+        logit = np.dot(inputs, self.weights) + self.bias
+        output = self.function(logit)
+        return logit
